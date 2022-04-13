a559bd07be3c5504c4aabd27809416eaae3e8c6f
DMYR2022 <103372844+DMYR2022@users.noreply.github.com>
2022-04-13 19:04:48 +0300

    Add files via upload

---
+++ hillel1.py
@@ -0,0 +1,12 @@
+age = int(input("Enter your age"))
+if age < 7:
+    print ('where are your parens')
+elif age >= 7 and age <16:
+    print ('thats the film for adult!')
+elif age > 65:
+    print ('where is your pension ID')
+elif age ==11 or age ==22 or age==33 or age==44 or age==55:
+    print ('what an interesting age!') 
+else:
+    print ('there are not tickects avaliable')
+
